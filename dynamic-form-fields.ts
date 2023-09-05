9d146a3d1db4cd9f994b470f0c82c26f7aa94993
Anush Shukla <34502156+anushshukla@users.noreply.github.com>
2023-09-06 03:08:02 +0530

    Create dynamic-form-fields.ts

---
+++ dynamic-form-fields.ts
@@ -0,0 +1,37 @@
+interface Field
+{
+    fieldId: number,
+    inputType: string,
+    label: string,
+    type: string,
+    required: boolean,
+    max: number;
+    min: number;
+    regex: string; 
+}
+
+interface Form
+{
+    formId: number;
+    fields: Field[];
+    isLoginRequired: boolean;
+    canResubmit: boolean;
+    canReceiveCopy: boolean;
+}
+
+enum FormSubmissionStatuses {
+    DRAFT = 'DRAFT',
+    SUBMITTED = 'SUBMITTED',
+}
+
+interface FilledField extends Pick<Field, 'fieldId' | 'label'> {
+ value: string;
+}
+
+interface FormSubmission
+{
+    userId: number;
+    formId: number;
+    status: FormSubmissionStatuses;
+    fields: FilledField[]
+}
